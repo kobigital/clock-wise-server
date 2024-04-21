ecc37d2e587a8b4448338463e41e83c4f0aef487
kobigital <165841033+kobigital@users.noreply.github.com>
2024-04-22 00:58:17 +0300

    fix referer

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,4 @@
-import express, { query } from 'express';
+import express from 'express';
 import passport from 'passport';
 
 const router = express.Router();
@@ -13,18 +13,13 @@ router.get(
     '/google/callback',
     passport.authenticate('google', { failureRedirect: '/?error=failureRedirect' }),
     function (req, res) {
-        const referer = req.headers.referer;
         const token = (req.user as any).token;
         const state = req.query.state as string;
         const extensionId = state?.slice(0, 4) === 'EXT-' ? state.slice(4) : undefined;
         if (extensionId) {
             return res.redirect(`https://${extensionId}.chromiumapp.org/login?token=${token}`)
-        } else if (referer) {
-            const refererUrl = new URL(referer);
-            const redirectTo = `${refererUrl.protocol}//${refererUrl.host}/login?token=${token}`;
-            return res.redirect(redirectTo);
         }
-        res.redirect(`http://localhost:3000/login?token=${token}`);
+        res.redirect(`${process.env.APP_URL}/login?token=${token}`);
     }
 );
 
